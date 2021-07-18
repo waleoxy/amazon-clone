adbe0920225415d8abf56dd2294a3129c6ee2765
Olawale Otubu <waleoxy@gmail.com>
2021-07-18 22:20:24 +0100

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            I am from home component
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBuUmQ4u7JWLfIrD335ATU4dRID-3dYfto",
+    authDomain: "amzone-clone-b8f5f.firebaseapp.com",
+    projectId: "amzone-clone-b8f5f",
+    storageBucket: "amzone-clone-b8f5f.appspot.com",
+    messagingSenderId: "799683639137",
+    appId: "1:799683639137:web:1eccfbcaf65688b292b720",
+    measurementId: "G-E21D91W13G"
+  };
