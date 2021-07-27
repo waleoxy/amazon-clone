1aeb4a08a0c3b62951ad224f65de92eaf1f9b618
Olawale Otubu <waleoxy@gmail.com>
2021-07-27 14:34:06 +0100

    index,js import removed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const { response, request } = require("express");
 const stripe = require("stripe")("sk_test_51JHSHFDR7pXVfLP8lHbKEhCAUffhdPoVlhjWpvZCCuGP6dtxXIiG0wYQmmZYR8iN9KigAAt1yZtA42R8h3tqfHPs00uUwg7Nzm");
 
 // API
