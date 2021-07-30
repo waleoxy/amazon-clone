ac9a6ef6ad11f6dd8bedbe1d04db5a975fe8a091
Olawale Otubu <waleoxy@gmail.com>
2021-07-30 22:29:11 +0100

    https

--- src/context/axios.js
+++ src/context/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/amzone-clone-b8f5f/us-central1/api"
+    baseURL: "https://localhost:5001/amzone-clone-b8f5f/us-central1/api"
 });
 export default instance;
